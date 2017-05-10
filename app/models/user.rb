9a14515126f4f3f7e03bc4c746fa8f8a6e443843
jeremira <jmiraille@gmail.com>
2017-05-10 10:41:28 +0900

    Finish Instagramm Omniauth login implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable,
          :omniauthable, :omniauth_providers => [:facebook, :instagram]
 
-  def self.from_omniauth(auth)
+  def self.from_fbomniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
      user.email = auth.info.email
      user.password = Devise.friendly_token[0,20]
@@ -14,7 +14,19 @@ class User < ApplicationRecord
      # If you are using confirmable and the provider(s) you use validate emails,
      # uncomment the line below to skip the confirmation emails.
      # user.skip_confirmation!
+   end
   end
-end
+
+  def self.from_igomniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+     user.email = 'temp@changeme.com'
+     user.password = Devise.friendly_token[0,20]
+     #user.name = auth.info.name   # assuming the user model has a name
+     #user.image = auth.info.image # assuming the user model has an image
+     # If you are using confirmable and the provider(s) you use validate emails,
+     # uncomment the line below to skip the confirmation emails.
+     # user.skip_confirmation!
+   end
+  end
 
 end
