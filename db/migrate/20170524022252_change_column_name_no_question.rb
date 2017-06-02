5d4866f14b8ff2228a60c9c28ff1954c1668066c
jeremira <jmiraille@gmail.com>
2017-06-02 12:24:58 +0900

    Clean database

--- db/migrate/20170524022252_change_column_name_no_question.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnNameNoQuestion < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :hotels , :todo?,        :todo
-    rename_column :restos,  :lunch_todo?,  :lunch_todo
-    rename_column :restos,  :dinner_todo?, :dinner_todo
-  end
-end
--- db/migrate/20170524022254_drop_resto.rb
+++
@@ -1,5 +0,0 @@
-class DropResto < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :restos
-  end
-end
