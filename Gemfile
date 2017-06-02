44d55fd77d757352eb6f9a684f004664a3158911
jeremira <jmiraille@gmail.com>
2017-06-02 12:56:44 +0900

    Add and setup Rspec/Capy

--- Gemfile
+++ Gemfile
@@ -58,6 +58,9 @@ gem 'figaro'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  #Rspec gem from tutorial : https://everydayrails.com/2012/03/12/testing-series-rspec-setup.html
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
 
 group :development do
@@ -67,6 +70,13 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'faker'
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem 'launchy'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,27 @@
 require_relative 'boot'
-
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-
 
 module Hyrule
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #Use Rspec to generate starter file for application test suite
+    #everydayrails.com rspec tutorial
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures         => true, #generate fixture for each model
+        :view_specs       => false, #skip generating view specs
+        :helper_specs     => false, #skip generating helper files
+        :routing_specs    => false, #skip generating helper for routes, TODOlater ?
+        :controller_specs => true,
+        :request_specs    => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories" #generate factory instead of default fixtures
+    end
   end
+
 end
