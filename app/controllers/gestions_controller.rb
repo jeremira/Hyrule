77628292da5e71249e76b3b6680c51131c482988
jeremira <jmiraille@gmail.com>
2017-06-18 13:13:17 +0900

    Add pending/approved option

--- app/controllers/gestions_controller.rb
+++ app/controllers/gestions_controller.rb
@@ -1,12 +1,33 @@
 class GestionsController < ApplicationController
+  before_action :set_trip_and_gestion
 
   def update
     case params[:status]
     when 'new'
 
     when 'pending'
+      if current_user == @trip.user && @gestion.status == 'new'
+        @gestion.status = 'pending'
+        if @gestion.save
+          flash[:notice] = "Thank you, we will now review your trip."
+        else
+          flash[:alert] = "Could not save change to this trip."
+        end
+      else
+        flash[:alert] = "You're not allowed to do this change."
+      end
 
     when 'approved'
+      if current_user.admin && @gestion.status == 'pending'
+        @gestion.status = 'approved'
+        if @gestion.save
+          flash[:notice] = "This trip has been approved."
+        else
+          flash[:alert] = "Could not save change to this trip."
+        end
+      else
+        flash[:alert] = "You're not allowed to do this change."
+      end
 
     when 'payed'
 
@@ -19,5 +40,11 @@ class GestionsController < ApplicationController
     end
     redirect_back(fallback_location: root_url)
   end
+
+  private
+    def set_trip_and_gestion
+      @trip    = Trip.find(params[:trip_id])
+      @gestion = @trip.gestion
+    end
 
 end
--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -2,7 +2,12 @@ class SetupController < ApplicationController
   before_action :only_admin
 
   def index
-    @pending_trips = Trip.all
+    @pending_trips = []
+    Trip.all.each do |trip|  #TODO has to be done with an activerecord query, thats shit
+      if trip.gestion.status == 'pending'
+        @pending_trips << trip
+      end
+    end
   end
 
 end
