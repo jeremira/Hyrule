2fdc9b5f674fd625e578b26a63e95144107c60eb
jeremira <jmiraille@gmail.com>
2017-06-30 13:56:03 +0900

    Reset CSS

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,3 @@
 $(document).on('turbolinks:load', function() {
 
-  console.log('launched');
-  //Todo: find vertical image for mobile, to charge if width < to minwidth
-  //$('body').css('background-image', 'url(assets/toulouse.jpeg)');
-
 }); //$ready function ending
