62966e5019d2e646f9e971335c6c597b57ee3424
jeremira <jmiraille@gmail.com>
2017-07-13 18:05:36 +0900

    Trip form done

--- app/models/rythme.rb
+++ app/models/rythme.rb
@@ -1,8 +1,6 @@
 class Rythme < ApplicationRecord
 
   validates :value,     presence: true
-  validates :walking,   presence: true
-  validates :transport, presence: true
 
   belongs_to :trip, required: false
 end
