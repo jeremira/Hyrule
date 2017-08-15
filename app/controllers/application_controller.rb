cf8e6d624a8fb1e31c232306672294c55fe77bc4
jeremira <jmiraille@gmail.com>
2017-08-15 18:05:15 +0900

    HTML content for trips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  #add email_confirmation strong params to devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def only_admin
     unless current_user && current_user.admin
       flash[:alert] = "Cette section est réservé aux administrateurs."
       redirect_to root_url
     end
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email_confirmation])
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :trips, :dependent => :destroy
 
   validates :email, presence: true
+  validates :email, confirmation: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
