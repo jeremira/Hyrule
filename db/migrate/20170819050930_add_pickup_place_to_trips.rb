c924ddabee188f00b71ea455777edbbfb51f62d7
jeremira <jmiraille@gmail.com>
2017-08-21 18:52:27 +0900

    Update devise FR mesage

--- db/migrate/20170819050930_add_pickup_place_to_trips.rb
+++
@@ -1,5 +0,0 @@
-class AddPickupPlaceToTrips < ActiveRecord::Migration[5.0]
-  def change
-    add_column :trips, :Pickup_place, :string
-  end
-end
--- db/migrate/20170819051552_delete_pickup_place_from_trips.rb
+++
@@ -1,5 +0,0 @@
-class DeletePickupPlaceFromTrips < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :trips, :Pickup_place, :string
-  end
-end
