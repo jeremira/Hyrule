439a9a84c67e33a82d20d32f5c583ac71d2638e1
jeremira <jmiraille@gmail.com>
2017-08-23 13:35:59 +0900

    change gmail port smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
     # SMTP settings for gmail
     config.action_mailer.smtp_settings = {
+      tls: true,
       :address              => "smtp.gmail.com",
-      :port                 => 587,
+      :port                 => 465,
       :domain               => "gmail.com",
       :user_name            => ENV['GMAIL_USERNAME'],
       :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => :plain,
+      :authentication       => 'plain',
       :enable_starttls_auto => true
     }
+
   config.action_mailer.default_url_options = { :host => 'http://www.tokyhop.com/' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
