7a0b27b295fe8ab1447e2299cd59757f086b969f
jeremira <jmiraille@gmail.com>
2017-08-23 13:29:36 +0900

    fix to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,10 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
+      :domain               => "gmail.com",
       :user_name            => ENV['GMAIL_USERNAME'],
       :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => "login",
+      :authentication       => :plain,
       :enable_starttls_auto => true
     }
   config.action_mailer.default_url_options = { :host => 'http://www.tokyhop.com/' }
