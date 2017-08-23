0b05ebe7301fd9b4b37cb9b95e0f55c230865ba1
jeremira <jmiraille@gmail.com>
2017-08-23 13:22:52 +0900

    Change mailer prod authentification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,10 @@ Rails.application.configure do
       :port                 => 587,
       :user_name            => ENV['GMAIL_USERNAME'],
       :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => "plain",
+      :authentication       => "login",
       :enable_starttls_auto => true
     }
-  config.action_mailer.default_url_options = { :host => 'https://evening-reaches-25835.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://www.tokyhop.com/' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
