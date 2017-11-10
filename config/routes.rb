841d79d9a01c6caf690115dbe9833abfbd8431d6
jeremira <jmiraille@gmail.com>
2017-11-10 14:55:00 +0900

    Remove Account controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
-  resources :accounts
   resources :setup, :only => [:index]
   resources :livrets do
     resources :assets
